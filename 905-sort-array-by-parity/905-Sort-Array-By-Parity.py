59707aae1858691be1b83f798b95132da2868f03
Sahil-coder50 <sahilsheoran24@gmail.com>
2026-09-19 16:51:07 +0530

    [905] [Time Beats: 47.67%] [Memory Beats: 79.54%] - LeetPush

---
+++ 905-sort-array-by-parity/905-Sort-Array-By-Parity.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArrayByParity(self, nums: list[int]) -> list[int]:
+        
+        n = len(nums)
+        start = 0
+
+        for i in range(n):
+            if nums[i] %2 == 0:
+                nums[i], nums[start] = nums[start], nums[i]
+                start+=1
+        
+        return nums
